a13f25ed59174943e04b17641a07c29b8c91ea34
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2023-04-22 21:53:51 +0530

    mail-template-added

--- back-end/mail.py
+++ back-end/mail.py
@@ -15,9 +15,22 @@ def send_mail(data,user):
     em['From']=data['mail']
     em['To']=user
     em['Subject']=data['subject']
-    body="""Mail from {}\n\nMail: {}\n\nMessage:\n{}\n\n\nRegards,\nMail Merchant Team""".format(data['name'],data['mail'],data['message'])
+    body="""
+        <div style="border:1px solid black; padding:20px;background-color:black;color:#FF6E31;">
+            <div>
+                <h2>Name: <span style="font-size:20px; font-weight: normal; color:#fff;">{}</span></h2>
+                <h2>Mail: <a href='mailto:{}' style="font-size:20px; font-weight: normal; color:#fff;">{}</a></h2>
+                <h2>Message: </h2>
+                <h3><span style="font-size:20px; font-weight: normal; color:#fff;">{}</span></h3>
+            </div>
+            <div style="font-size:10px; border-width:1px 0px 0px 0px;border-style: solid; padding:5px; width:100%; margin-top:100px;">
+                <span>Regards,<br/> Mail Merchant Team</span><br/>
+                <span><a href='mailto:mailmerchant1018@gmail.com' style='color:#FF6E31;'>mailmerchant1018@gmail.com</a></span>
+            </div>
+        </div>
+    """.format(data['name'],data['mail'],data['mail'],data['message'])
     em.set_content(body)
-    
+    em.set_type('text/html')
     cntxt=ssl.create_default_context()
     try:
         
