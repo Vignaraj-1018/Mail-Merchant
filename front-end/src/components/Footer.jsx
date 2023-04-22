fc931091bf9c8187e8f5e8d86b779401c9e1381d
Vignaraj D <86393989+Vignaraj-1018@users.noreply.github.com>
2023-04-22 09:06:47 +0530

    footer-edit

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className=' bg-body text-white font-semibold w-full bottom-0'>
-      <span className='flex text-sm justify-center items-center'>Copyright Reserved @ 2023</span>
+      <span className='flex text-sm justify-center items-center'> &#169; All Rights Reserved @ 2023</span>
     </div>
   )
 }
